bf0fde463b0ef67f32b5cfcc86e0fa51acf4bc08
s-peiran <she.peiran@gmail.com>
2024-06-04 09:33:25 +0800

    Updated Note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ function Note() {
   return (
     <div className="note_container">
     <div className="note">
-      <h1 style={{ textAlign: 'center' }}>Our Website is under maintenance from 3/6/2024 to 4/6/2024</h1>
+      <h1 style={{ textAlign: 'center' }}>Our Website is currently under maintenance</h1>
       <p style={{ textAlign: 'center' }}>Sorry for any incovenience caused</p>
     </div>
     </div>
